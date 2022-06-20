f8fce7ff85efcad47ef1f9920a13f9fbb0707a80
NANDITHA <NANDITHA@LAPTOP-EV9T0HBD>
2022-06-20 17:11:20 +0530

    Add project files.

---
+++ CalculatorLib/Class1.cs
@@ -0,0 +1,46 @@
+﻿//namespace name
+namespace CalculatorLib
+{
+    //access modifier class classname
+    public class CalculateOperations
+    {
+        //class members
+        //fields
+        //properties
+        //methods
+        //contructor
+        //destructor
+        //events
+        //indexes
+        //other child classes
+
+        //Add
+
+        //access modifier returntype methodname (parameters){ // your implementation}
+        public int Add(int a, int b)
+        {
+            int c = a + b;
+            return c;
+        }
+        public int Sub(int a, int b)
+        {
+            int d = a * b;
+            return d;
+        }
+        public int Mul(int a, int b)
+        {
+            int e = a * b;
+            return e;
+        }
+        public int Div(int a, int b)
+        {
+            int f = a / b;
+            return f;
+        }
+
+    }
+
+
+
+
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CalculatorLib;
+CalculateOperations operations = new CalculateOperations();
+int ra = operations.Add(10, 10);
+int rs = operations.Sub(10, 10);
+int rm = operations.Mul(10, 10);
+int rd = operations.Div(10, 10);
+Console.WriteLine("Addition: "+ra);
+Console.WriteLine("Subtraction: "+rs);
+Console.WriteLine("Multiplication: " + rm);
+Console.WriteLine("Division: " + rd);
+Console.ReadLine();
+
